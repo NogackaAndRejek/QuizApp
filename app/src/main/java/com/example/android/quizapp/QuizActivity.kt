0d497404af8a39cb98d949ef5167ef607e049142
Rejfi <rrafaul@wp.pl>
2018-08-22 18:08:03 +0200

    Checking connection with GitHub

--- app/src/main/java/com/example/android/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/android/quizapp/QuizActivity.kt
@@ -2,11 +2,18 @@ package com.example.android.quizapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Toast.makeText(applicationContext,"Cheking connection with Github", Toast.LENGTH_SHORT).show()
     }
 }
