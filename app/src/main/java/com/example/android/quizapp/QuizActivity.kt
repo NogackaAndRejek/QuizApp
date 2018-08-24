0babe739d748d004b1099db9b57aa953eb749746
Rejfi <rrafaul@wp.pl>
2018-08-24 22:56:03 +0200

    Commit #1 Before pull

--- app/src/main/java/com/example/android/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/android/quizapp/QuizActivity.kt
@@ -2,11 +2,22 @@ package com.example.android.quizapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Chronometer
+import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        timer.format = "%m"
+
     }
 }
