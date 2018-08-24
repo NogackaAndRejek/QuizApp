a5c5e1e2238baf4a36931acb5fe6dccd75e1f855
Rejfi <rrafaul@wp.pl>
2018-08-24 23:44:27 +0200

    New menu layout

--- app/src/main/java/com/example/android/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/android/quizapp/QuizActivity.kt
@@ -1,23 +1,18 @@
 package com.example.android.quizapp
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Chronometer
-import kotlinx.android.synthetic.main.activity_quiz.*
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
-
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        timer.format = "%m"
-
     }
 }
