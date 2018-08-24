1bfc1075a63aa47f2045c6a4b1c6e8556e9ae066
Karolina Nogacka <nogacka.karolina@gmail.com>
2018-08-24 16:51:30 +0200

    Checking is kotlin working fine

--- app/src/main/java/com/example/android/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/android/quizapp/QuizActivity.kt
@@ -1,12 +1,23 @@
 package com.example.android.quizapp
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_quiz.*
 
 class QuizActivity : AppCompatActivity() {
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        var answerAButton = findViewById<Button>(R.id.answareA)
+        answerAButton.setOnClickListener{
+            answerAButton.setBackgroundColor(R.color.buttonColor)
+        }
     }
+
 }
