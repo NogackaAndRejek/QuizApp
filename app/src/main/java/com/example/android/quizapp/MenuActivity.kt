48779ec7bb144d592f3b8fb63955e256d6ef73a9
Rejfi <rrafaul@wp.pl>
2018-08-24 23:56:43 +0200

    Changes in Android Manifest. New category.Launcher

---
+++ app/src/main/java/com/example/android/quizapp/MenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.quizapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
