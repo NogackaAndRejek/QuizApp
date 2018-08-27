cb4b1590c07d2fedda6444d44971ca8e419232dd
Karolina Nogacka <nogacka.karolina@gmail.com>
2018-08-27 21:39:02 +0200

    Add Question class

---
+++ app/src/main/java/com/example/android/quizapp/Question.kt
@@ -0,0 +1,25 @@
+package com.example.android.quizapp
+
+/**
+ * Question
+ * Class stores question instance in quiz
+ */
+
+class Question(var question: String,
+               var answersA: String,
+               var answersB: String,
+               var answersC: String,
+               var answersD: String,
+               var correctAnswer: Char,
+               var questionImage: Int){
+
+    /**
+     * isAnswerCorrect
+     * Is player answer correct
+     * @param playerAnswer
+     * @return Boolean (True or False) is player's answer correct
+     */
+    fun isAnswerCorrect(playerAnswer: Char): Boolean {
+        return playerAnswer == correctAnswer
+    }
+}
